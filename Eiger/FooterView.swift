ee1ce988f734247935be81194478ef0f9a9343c8
temma.io <eifan.devs@gmail.com>
2017-03-05 03:23:43 +0900

    FooterView作成

---
+++ Eiger/FooterView.swift
@@ -0,0 +1,22 @@
+//
+//  FooterView.swift
+//  Eiger
+//
+//  Created by temma on 2017/03/05.
+//  Copyright © 2017年 eifaniori. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FooterView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor.frenchBlue
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
