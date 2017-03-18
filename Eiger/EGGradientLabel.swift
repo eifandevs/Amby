0997610f2c096d6f1681663dfa46d8f43540e9ca
temma.io <eifan.devs@gmail.com>
2017-03-18 15:33:15 +0900

    ヘッダーラベルにグラデーションを付与

--- Eiger/EGGradientLabel.swift
+++ Eiger/EGGradientLabel.swift
@@ -10,8 +10,20 @@ import Foundation
 import UIKit
 
 class EGGradientLabel: UILabel {
+    let gradient = CAGradientLayer.init()
+    
     init() {
         super.init(frame: CGRect.zero)
+        gradient.colors = [
+            UIColor.init(red: 1, green: 1, blue: 1, alpha: 0).cgColor,
+            UIColor.init(red: 1, green: 1, blue: 1, alpha: 1).cgColor]
+        gradient.startPoint = CGPoint(x: 0.6, y: 0.0)
+        gradient.endPoint = CGPoint(x: 1, y: 0.0)
+        layer.addSublayer(gradient)
+    }
+    
+    override func layoutSubviews() {
+        gradient.frame = CGRect(x: 0.0, y: 0.0, width: frame.size.width, height: frame.size.height)
     }
     
     required init?(coder aDecoder: NSCoder) {
