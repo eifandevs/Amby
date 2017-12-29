34858f69be815c494bb6314a00d3dbf927df8697
temma.io <eifan.devs@gmail.com>
2017-12-30 04:21:08 +0900

    スライダーを実装

--- Qas/View/Base/FrontLayer/OptionMenu/Cell/OptionMenuSettingSliderTableViewCell.swift
+++ Qas/View/Base/FrontLayer/OptionMenu/Cell/OptionMenuSettingSliderTableViewCell.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class OptionMenuSettingSliderTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var slider: UISlider!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        slider.minimumValue = Float(-0.07)
+        slider.maximumValue = Float(-0.01)
+        slider.value = -UserDefaults.standard.float(forKey: AppConst.KEY_AUTO_SCROLL_INTERVAL)
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -21,4 +27,7 @@ class OptionMenuSettingSliderTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    @IBAction func changedValue(_ sender: Any) {
+        UserDefaults.standard.set(-(slider.value), forKey: AppConst.KEY_AUTO_SCROLL_INTERVAL)
+    }
 }
