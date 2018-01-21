9f4ff467857fddba21fc33accf0ee69a9c061668
temma.io <eifan.devs@gmail.com>
2018-01-21 19:49:40 +0900

    インサートアニメーション調整

--- Qas/View/Base/BaseLayer/Footer/FooterView.swift
+++ Qas/View/Base/BaseLayer/Footer/FooterView.swift
@@ -152,7 +152,7 @@ class FooterView: UIView, ShadowView {
         scrollView.addSubview(btn)
 
         // 挿入アニメーション
-        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseIn, animations: {
+        UIView.animate(withDuration: 0.3, delay: 0.2, options: .curveLinear, animations: {
             btn.frame.origin.y = 0
         }, completion: nil)
         
