f07ac9c91a282b6181c6792894dccaa40ffd2001
temma.io <eifan.devs@gmail.com>
2018-02-04 07:28:57 +0900

    フッターの通知イベント修正

--- Qas/ViewModel/BaseLayer/FooterViewModel.swift
+++ Qas/ViewModel/BaseLayer/FooterViewModel.swift
@@ -7,6 +7,8 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 protocol FooterViewModelDelegate: class {
     func footerViewModelDidAppendThumbnail(pageHistory: PageHistory)
@@ -40,62 +42,90 @@ class FooterViewModel {
     /// 通知センター
     let center = NotificationCenter.default
     
+    /// Observable自動解放
+    let disposeBag = DisposeBag()
+    
     init(index: Int) {
         // webview追加
-        center.addObserver(forName: .pageHistoryDataModelDidAppend, object: nil, queue: nil) { [weak self] (notification) in
-            guard let `self` = self else { return }
-            log.debug("[Footer Event]: pageHistoryDataModelDidAppend")
-            let pageHistory = (notification.object as! [String: Any])[AppConst.KEY_NOTIFICATION_OBJECT] as! PageHistory
-            
-            // 新しいサムネイルを追加
-            self.delegate?.footerViewModelDidAppendThumbnail(pageHistory: pageHistory)
-        }
-        // webview挿入
-        center.addObserver(forName: .pageHistoryDataModelDidInsert, object: nil, queue: nil) { [weak self] (notification) in
-            guard let `self` = self else { return }
-            log.debug("[Footer Event]: pageHistoryDataModelDidInsert")
-            let pageHistory = (notification.object as! [String: Any])[AppConst.KEY_NOTIFICATION_OBJECT] as! PageHistory
-            let at = (notification.object as! [String: Any])[AppConst.KEY_NOTIFICATION_AT] as! Int
-
-            // 新しいサムネイルを追加
-            self.delegate?.footerViewModelDidInsertThumbnail(at: at, pageHistory: pageHistory)
-        }
-        // webviewロード開始
-        center.addObserver(forName: .pageHistoryDataModelDidStartLoading, object: nil, queue: nil) { [weak self] (notification) in
-            guard let `self` = self else { return }
-            log.debug("[Footer Event]: pageHistoryDataModelDidStartLoading")
-            // FooterViewに通知をする
-            self.delegate?.footerViewModelDidStartLoading(context: notification.object as! String)
-        }
-        // webviewレンダリング完了
-        center.addObserver(forName: .pageHistoryDataModelDidEndRendering, object: nil, queue: nil) { [weak self] (notification) in
-            guard let `self` = self else { return }
-            log.debug("[Footer Event]: pageHistoryDataModelDidEndRendering")
-            // FooterViewに通知をする
-            let context = notification.object as! String
-            if let pageHistory = D.find(PageHistoryDataModel.s.histories, callback: { $0.context == context }) {
-                self.delegate?.footerViewModelDidEndLoading(context: context, title: pageHistory.title)
+        center.rx.notification(.pageHistoryDataModelDidAppend, object: nil)
+            .subscribe { [weak self] notification in
+                guard let `self` = self else { return }
+                log.debug("[FooterViewModel Event]: pageHistoryDataModelDidAppend")
+                if let notification = notification.element {
+                    let pageHistory = (notification.object as! [String: Any])[AppConst.KEY_NOTIFICATION_OBJECT] as! PageHistory
+                    // 新しいサムネイルを追加
+                    self.delegate?.footerViewModelDidAppendThumbnail(pageHistory: pageHistory)
+                }
             }
-        }
+            .disposed(by: disposeBag)
+
+        // webview挿入
+        center.rx.notification(.pageHistoryDataModelDidInsert, object: nil)
+            .subscribe { [weak self] notification in
+                guard let `self` = self else { return }
+                log.debug("[FooterViewModel Event]: pageHistoryDataModelDidInsert")
+                if let notification = notification.element {
+                    let pageHistory = (notification.object as! [String: Any])[AppConst.KEY_NOTIFICATION_OBJECT] as! PageHistory
+                    let at = (notification.object as! [String: Any])[AppConst.KEY_NOTIFICATION_AT] as! Int
+                    // 新しいサムネイルを追加
+                    self.delegate?.footerViewModelDidInsertThumbnail(at: at, pageHistory: pageHistory)
+                }
+            }
+            .disposed(by: disposeBag)
+
+        // webviewロード開始
+        center.rx.notification(.pageHistoryDataModelDidStartLoading, object: nil)
+            .subscribe { [weak self] notification in
+                guard let `self` = self else { return }
+                log.debug("[FooterViewModel Event]: pageHistoryDataModelDidStartLoading")
+                if let notification = notification.element {
+                    // FooterViewに通知をする
+                    self.delegate?.footerViewModelDidStartLoading(context: notification.object as! String)
+                }
+            }
+            .disposed(by: disposeBag)
+
+        // webviewレンダリング完了
+        center.rx.notification(.pageHistoryDataModelDidEndRendering, object: nil)
+            .subscribe { [weak self] notification in
+                guard let `self` = self else { return }
+                log.debug("[FooterViewModel Event]: pageHistoryDataModelDidEndRendering")
+                if let notification = notification.element {
+                    // FooterViewに通知をする
+                    let context = notification.object as! String
+                    if let pageHistory = D.find(PageHistoryDataModel.s.histories, callback: { $0.context == context }) {
+                        self.delegate?.footerViewModelDidEndLoading(context: context, title: pageHistory.title)
+                    }
+                }
+            }
+            .disposed(by: disposeBag)
 
         // webview切り替え
-        center.addObserver(forName: .pageHistoryDataModelDidChange, object: nil, queue: nil) { [weak self] (notification) in
-            guard let `self` = self else { return }
-            log.debug("[Footer Event]: pageHistoryDataModelDidChange")
-            self.delegate?.footerViewModelDidChangeThumbnail(context: notification.object as! String)
-        }
+        center.rx.notification(.pageHistoryDataModelDidChange, object: nil)
+            .subscribe { [weak self] notification in
+                guard let `self` = self else { return }
+                log.debug("[FooterViewModel Event]: pageHistoryDataModelDidChange")
+                if let notification = notification.element {
+                    self.delegate?.footerViewModelDidChangeThumbnail(context: notification.object as! String)
+                }
+            }
+            .disposed(by: disposeBag)
         
         // webview削除
-        center.addObserver(forName: .pageHistoryDataModelDidRemove, object: nil, queue: nil) { [weak self] (notification) in
-            guard let `self` = self else { return }
-            log.debug("[Footer Event]: pageHistoryDataModelDidRemove")
-            let context = (notification.object as! [String: Any])["context"] as! String
-            let pageExist = (notification.object as! [String: Any])["pageExist"] as! Bool
-            
-            // 実データの削除
-            try! FileManager.default.removeItem(atPath: Util.thumbnailFolderUrl(folder: context).path)
-            self.delegate?.footerViewModelDidRemoveThumbnail(context: context, pageExist: pageExist)
-        }
+        center.rx.notification(.pageHistoryDataModelDidRemove, object: nil)
+            .subscribe { [weak self] notification in
+                guard let `self` = self else { return }
+                log.debug("[FooterViewModel Event]: pageHistoryDataModelDidRemove")
+                if let notification = notification.element {
+                    let context = (notification.object as! [String: Any])["context"] as! String
+                    let pageExist = (notification.object as! [String: Any])["pageExist"] as! Bool
+                    
+                    // 実データの削除
+                    try! FileManager.default.removeItem(atPath: Util.thumbnailFolderUrl(folder: context).path)
+                    self.delegate?.footerViewModelDidRemoveThumbnail(context: context, pageExist: pageExist)
+                }
+            }
+            .disposed(by: disposeBag)
     }
     
     deinit {
