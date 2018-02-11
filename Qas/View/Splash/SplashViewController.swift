248bba4da35001104e7b7c7c3722c6eefa39d6c0
temma.io <eifan.devs@gmail.com>
2018-02-11 22:59:00 +0900

    ビルドエラーを修正

--- Qas/View/Splash/SplashViewController.swift
+++ Qas/View/Splash/SplashViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import LTMorphingLabel
 
 protocol SplashViewControllerDelegate: class {
     func splashViewControllerDidEndDrawing()
@@ -16,31 +15,16 @@ protocol SplashViewControllerDelegate: class {
 class SplashViewController: UIViewController {
 
     @IBOutlet var contentView: UIView!
-    @IBOutlet weak var splashLabel: LTMorphingLabel!
     
     weak var delegate: SplashViewControllerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         delegate?.splashViewControllerDidEndDrawing()
-
-//        splashLabel.frame.size = CGSize(width: DeviceConst.DISPLAY_SIZE.width / 1.2, height: DeviceConst.DISPLAY_SIZE.height / 18)
-//        splashLabel.center = CGPoint(x: DeviceConst.DISPLAY_SIZE.width / 2, y: DeviceConst.DISPLAY_SIZE.height / 2)
-//        splashLabel.morphingEffect = .scale
-//        splashLabel.delegate = self
-//        splashLabel.font = UIFont(name: splashLabel.font.fontName, size: splashLabel.frame.size.height / 2)
-//        splashLabel.text = "Quick AcceSs browser"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-}
-
-// MARK: LTMorphingLabel Delegate
-extension SplashViewController: LTMorphingLabelDelegate {
-    func morphingDidComplete(_ label: LTMorphingLabel) {
-        delegate?.splashViewControllerDidEndDrawing()
     }
 }
