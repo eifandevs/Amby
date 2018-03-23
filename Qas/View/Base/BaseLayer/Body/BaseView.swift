5589d2b49092f58e44138418788d07fbcc83f49f
temma.io <eifan.devs@gmail.com>
2018-03-23 21:46:19 +0900

    warning修正

--- Qas/View/Base/BaseLayer/Body/BaseView.swift
+++ Qas/View/Base/BaseLayer/Body/BaseView.swift
@@ -620,7 +620,7 @@ extension BaseView: EGApplicationDelegate {
             if ((swipeDirection == .left && touchPoint.x > AppConst.FRONT_LAYER_EDGE_SWIPE_EREA + 20) ||
                 (swipeDirection == .right && touchPoint.x < self.bounds.width - AppConst.FRONT_LAYER_EDGE_SWIPE_EREA - 20)) {
                 // エッジスワイプ検知
-                // TODO: 動画DL
+                // 動画DL
 //                self.front.evaluateJavaScript("document.querySelector('video').currentSrc") { (object, error) in
 //                    log.warning("object: \(object)")
 //                }
