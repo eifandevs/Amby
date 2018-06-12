c6aa4e9741ded2f3dc93fa5ed569c78e6312cfc2
temma.io <eifan.devs@gmail.com>
2018-06-13 07:12:08 +0900

    add ut

--- Qass-DevelopTests/Model/FavoriteDataModelTests.swift
+++ Qass-DevelopTests/Model/FavoriteDataModelTests.swift
@@ -48,4 +48,92 @@ class FavoriteDataModelTests: XCTestCase {
         self.waitForExpectations(timeout: 10, handler: nil)
     }
     
+    func testSelect() {
+        let fd = Favorite()
+        fd.title = #function
+        fd.url = #function
+        
+        weak var expectation = self.expectation(description: #function)
+        
+        FavoriteDataModel.s.rx_favoriteDataModelDidInsert
+            .subscribe { _ in
+                if let expectation = expectation {
+                    expectation.fulfill()
+                    XCTAssertTrue(FavoriteDataModel.s.select(id: fd.id).count > 0)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        FavoriteDataModel.s.insert(favorites: [fd])
+        
+        self.waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    func testDelete() {
+        let fd = Favorite()
+        fd.title = #function
+        fd.url = #function
+        
+        let fd2 = Favorite()
+        fd2.title = #function
+        fd2.url = #function
+        
+        FavoriteDataModel.s.insert(favorites: [fd, fd2])
+        FavoriteDataModel.s.delete()
+        
+        XCTAssertTrue(FavoriteDataModel.s.select().count == 0)
+    }
+    
+    func testDeletePartial() {
+        let fd = Favorite()
+        fd.title = #function
+        fd.url = #function
+        
+        let fd2 = Favorite()
+        fd2.title = #function
+        fd2.url = #function
+        
+        weak var expectation = self.expectation(description: #function)
+        
+        FavoriteDataModel.s.rx_favoriteDataModelDidRemove
+            .subscribe { _ in
+                if let expectation = expectation {
+                    expectation.fulfill()
+                    XCTAssertTrue(FavoriteDataModel.s.select().count == 1)
+                    XCTAssertTrue(FavoriteDataModel.s.select().first!.id == fd2.id)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        FavoriteDataModel.s.insert(favorites: [fd, fd2])
+        FavoriteDataModel.s.delete(favorites: [fd], notify: true)
+        
+        self.waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    func testDeleteAll() {
+        let fd = Favorite()
+        fd.title = #function
+        fd.url = #function
+        
+        let fd2 = Favorite()
+        fd2.title = #function
+        fd2.url = #function
+        
+        weak var expectation = self.expectation(description: #function)
+        
+        FavoriteDataModel.s.rx_favoriteDataModelDidRemove
+            .subscribe { _ in
+                if let expectation = expectation {
+                    expectation.fulfill()
+                    XCTAssertTrue(FavoriteDataModel.s.select().count == 0)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        FavoriteDataModel.s.insert(favorites: [fd, fd2])
+        FavoriteDataModel.s.delete(notify: true)
+        
+        self.waitForExpectations(timeout: 10, handler: nil)
+    }
 }
