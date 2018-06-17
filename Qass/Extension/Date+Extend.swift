dabf5ae586c0a364f2069ec858fc688b9ab7cabd
temma.io <eifan.devs@gmail.com>
2018-06-17 20:15:27 +0900

    add date extension

--- Qass/Extension/Date+Extend.swift
+++ Qass/Extension/Date+Extend.swift
@@ -16,4 +16,20 @@ extension Date {
         formatter.dateFormat = format
         return formatter.string(from: self)
     }
+    
+    var yesterday: Date {
+        return Calendar.current.date(byAdding: .day, value: -1, to: noon)!
+    }
+    var tomorrow: Date {
+        return Calendar.current.date(byAdding: .day, value: 1, to: noon)!
+    }
+    var noon: Date {
+        return Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: self)!
+    }
+    var month: Int {
+        return Calendar.current.component(.month, from: self)
+    }
+    var isLastDayOfMonth: Bool {
+        return tomorrow.month != month
+    }
 }
