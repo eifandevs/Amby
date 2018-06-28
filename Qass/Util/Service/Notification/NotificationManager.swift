004ca85fcc2379f425554832f56f63349e5d3dc9
temma.io <eifan.devs@gmail.com>
2018-06-28 21:20:18 +0900

    fix warning

--- Qass/Util/Service/Notification/NotificationManager.swift
+++ Qass/Util/Service/Notification/NotificationManager.swift
@@ -18,8 +18,10 @@ class NotificationManager {
         notificationView.titleLabel?.font = UIFont(name: AppConst.APP_FONT, size: notificationView.frame.size.height / 4)
         notificationView.backgroundColor = UIColor.darkGray
         notificationView.contentHorizontalAlignment = .left
-        (UIApplication.shared.delegate as! AppDelegate).window?.rootViewController?.view.addSubview(notificationView)
-        notificationView.play()
+        if let delegate = UIApplication.shared.delegate as? AppDelegate {
+            delegate.window?.rootViewController?.view.addSubview(notificationView)
+            notificationView.play()
+        }
     }
 
     static func presentAlert(title: String, message: String, completion: @escaping (() -> Void)) {
