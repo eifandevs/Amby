ca3d88cb951889b384f0f37d8737da2a6412ab01
temma.io <eifan.devs@gmail.com>
2018-07-13 08:10:21 +0900

    fix warning

--- Qass/View/Base/BaseLayer/Body/SearchMenuTableView.swift
+++ Qass/View/Base/BaseLayer/Body/SearchMenuTableView.swift
@@ -180,25 +180,31 @@ extension SearchMenuTableView: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.section == 0 {
             // オートコンプリート表示
-            let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.searchMenuSuggestCell.identifier, for: indexPath) as! SearchMenuSuggestTableViewCell
-            cell.setSuggest(suggest: viewModel.suggestCellItem[safe: indexPath.row])
-            return cell
+            if let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.searchMenuSuggestCell.identifier, for: indexPath) as? SearchMenuSuggestTableViewCell {
+                cell.setSuggest(suggest: viewModel.suggestCellItem[safe: indexPath.row])
+                return cell
+            }
         } else if indexPath.section == 1 {
             // 検索履歴表示
-            let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.searchMenuSearchHistoryCell.identifier, for: indexPath) as! SearchMenuSearchHistoryTableViewCell
-            cell.setHistory(history: viewModel.searchHistoryCellItem[safe: indexPath.row])
-            return cell
+            if let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.searchMenuSearchHistoryCell.identifier, for: indexPath) as? SearchMenuSearchHistoryTableViewCell {
+                cell.setHistory(history: viewModel.searchHistoryCellItem[safe: indexPath.row])
+                return cell
+            }
         } else if indexPath.section == 2 {
             // 閲覧履歴表示
-            let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.searchMenuCommonHistoryCell.identifier, for: indexPath) as! SearchMenuCommonHistoryTableViewCell
-            cell.setHistory(history: viewModel.commonHistoryCellItem[safe: indexPath.row])
-            return cell
+            if let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.searchMenuCommonHistoryCell.identifier, for: indexPath) as? SearchMenuCommonHistoryTableViewCell {
+                cell.setHistory(history: viewModel.commonHistoryCellItem[safe: indexPath.row])
+                return cell
+            }
         } else {
             // 記事表示
-            let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.searchMenuNewsCell.identifier, for: indexPath) as! SearchMenuNewsTableViewCell
-            cell.setArticle(article: viewModel.newsItem[safe: indexPath.row])
-            return cell
+            if let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.searchMenuNewsCell.identifier, for: indexPath) as? SearchMenuNewsTableViewCell {
+                cell.setArticle(article: viewModel.newsItem[safe: indexPath.row])
+                return cell
+            }
         }
+
+        return UITableViewCell()
     }
 
     func tableView(_: UITableView, titleForHeaderInSection section: Int) -> String? {
