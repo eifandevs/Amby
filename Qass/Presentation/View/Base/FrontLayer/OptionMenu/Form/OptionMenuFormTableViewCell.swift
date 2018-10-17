5ae82e7eaa2cadefc37f638f723d8de84b613216
temma.io <eifan.devs@gmail.com>
2018-10-18 08:47:04 +0900

    open button tap code in form tableview

--- Qass/Presentation/View/Base/FrontLayer/OptionMenu/Form/OptionMenuFormTableViewCell.swift
+++ Qass/Presentation/View/Base/FrontLayer/OptionMenu/Form/OptionMenuFormTableViewCell.swift
@@ -6,22 +6,38 @@
 //  Copyright © 2017年 eifaniori. All rights reserved.
 //
 
+import RxCocoa
+import RxSwift
 import UIKit
 
 class OptionMenuFormTableViewCell: UITableViewCell {
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var urlLabel: UILabel!
-    @IBOutlet weak var openButton: UIButton!
-    
+    @IBOutlet var openButton: UIButton!
+
+    /// Observable自動解放
+    private let disposeBag = DisposeBag()
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        setRx()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+
+    func setRx() {
+        openButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                log.eventIn(chain: "rx_tap")
+                guard let `self` = self else { return }
+                log.eventOut(chain: "rx_tap")
+            })
+            .disposed(by: disposeBag)
     }
 
     // ビューモデルデータ設定
