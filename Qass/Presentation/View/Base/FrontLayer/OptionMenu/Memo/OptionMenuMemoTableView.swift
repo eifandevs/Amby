a8286eb2f3b7f931425de361526cfc3cce3a3c33
temma.io <eifan.devs@gmail.com>
2018-10-19 08:45:32 +0900

    add memo tableview

---
+++ Qass/Presentation/View/Base/FrontLayer/OptionMenu/Memo/OptionMenuMemoTableView.swift
@@ -0,0 +1,21 @@
+//
+//  OptionMenuMemoTableView.swift
+//  Qass
+//
+//  Created by tenma on 2018/10/19.
+//  Copyright © 2018年 eifandevs. All rights reserved.
+//
+
+import UIKit
+
+class OptionMenuMemoTableView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
