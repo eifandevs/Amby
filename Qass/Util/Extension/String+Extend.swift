94b95725964517abcf4add463c06a099a0ed2460
temma.io <eifan.devs@gmail.com>
2018-10-24 09:08:14 +0900

    fix warning

--- Qass/Util/Extension/String+Extend.swift
+++ Qass/Util/Extension/String+Extend.swift
@@ -64,22 +64,6 @@ extension String {
         return index(startIndex, offsetBy: from)
     }
 
-    func substring(from: Int) -> String {
-        let fromIndex = index(from: from)
-        return substring(from: fromIndex)
-    }
-
-    func substring(to: Int) -> String {
-        let toIndex = index(from: to)
-        return substring(to: toIndex)
-    }
-
-    func substring(with r: Range<Int>) -> String {
-        let startIndex = index(from: r.lowerBound)
-        let endIndex = index(from: r.upperBound)
-        return substring(with: startIndex ..< endIndex)
-    }
-
     func encodeUrl() -> String? {
         if contains("%") {
             return self
