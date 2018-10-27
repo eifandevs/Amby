9b17dda4164e6d060e500800e36484d90a30abb4
temma.io <eifan.devs@gmail.com>
2018-10-27 22:50:31 +0900

    create memo and form vc

---
+++ Qass/Presentation/View/Form/FormViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FormViewController.swift
+//  Qass
+//
+//  Created by tenma on 2018/10/27.
+//  Copyright © 2018年 eifandevs. All rights reserved.
+//
+
+import UIKit
+
+class FormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
