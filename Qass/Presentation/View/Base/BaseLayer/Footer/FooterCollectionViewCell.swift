1a9142be8c1d9b26a0a0e25cc568a15b558f46e0
temma.io <eifan.devs@gmail.com>
2018-11-10 02:24:10 +0900

    add footerview xib

---
+++ Qass/Presentation/View/Base/BaseLayer/Footer/FooterCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  FooterCollectionViewCell.swift
+//  Qass
+//
+//  Created by tenma on 2018/11/10.
+//  Copyright © 2018年 eifandevs. All rights reserved.
+//
+
+import UIKit
+
+class FooterCollectionViewCell: UICollectionViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
