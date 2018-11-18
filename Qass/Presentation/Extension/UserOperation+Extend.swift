172c3937244d779ce0c0ef25d82b45aaa6c4f98b
temma.io <eifan.devs@gmail.com>
2018-11-18 18:28:03 +0900

    useroperation extend

--- Qass/Presentation/Extension/UserOperation+Extend.swift
+++ Qass/Presentation/Extension/UserOperation+Extend.swift
@@ -44,4 +44,82 @@ extension UserOperation {
             return "タブグループ"
         }
     }
+
+    func imageColor() -> UIColor {
+        return UIColor.darkGray
+    }
+
+    func image() -> UIImage {
+        switch self {
+        case .menu:
+            return R.image.circlemenuMenu()!
+        case .close:
+            return R.image.circlemenuClose()!
+        case .closeAll:
+            return R.image.circlemenuCloseAll()!
+        case .historyBack:
+            return R.image.circlemenuHistoryback()!
+        case .copy:
+            return R.image.circlemenuCopy()!
+        case .search:
+            return R.image.circlemenuSearch()!
+        case .grep:
+            return R.image.circlemenuGrep()!
+        case .add:
+            return R.image.circlemenuAdd()!
+        case .scrollUp:
+            return R.image.circlemenuScrollup()!
+        case .autoScroll:
+            return R.image.circlemenuAutoscroll()!
+        case .historyForward:
+            return R.image.circlemenuHistoryforward()!
+        case .form:
+            return R.image.circlemenuForm()!
+        case .favorite:
+            return R.image.circlemenuFavorite()!
+        case .urlCopy:
+            // TODO: URLコピーの画像
+            return R.image.circlemenuFavorite()!
+        case .tabGroup:
+            // TODO: タブグループの画像
+            return R.image.circlemenuFavorite()!
+        }
+    }
+
+    func accesibilityIdentify() -> String {
+        switch self {
+        case .menu:
+            return R.image.circlemenuMenu.name
+        case .close:
+            return R.image.circlemenuClose.name
+        case .closeAll:
+            return R.image.circlemenuCloseAll.name
+        case .historyBack:
+            return R.image.circlemenuHistoryback.name
+        case .copy:
+            return R.image.circlemenuCopy.name
+        case .search:
+            return R.image.circlemenuSearch.name
+        case .grep:
+            return R.image.circlemenuGrep.name
+        case .add:
+            return R.image.circlemenuAdd.name
+        case .scrollUp:
+            return R.image.circlemenuScrollup.name
+        case .autoScroll:
+            return R.image.circlemenuAutoscroll.name
+        case .historyForward:
+            return R.image.circlemenuHistoryforward.name
+        case .form:
+            return R.image.circlemenuForm.name
+        case .favorite:
+            return R.image.circlemenuFavorite.name
+        case .urlCopy:
+            // TODO: URLコピーの画像
+            return R.image.circlemenuFavorite.name
+        case .tabGroup:
+            // TODO: タブグループの画像
+            return R.image.circlemenuFavorite.name
+        }
+    }
 }
--- Qass/Presentation/View/Base/FrontLayer/CircleMenu/CircleMenuItem.swift
+++ Qass/Presentation/View/Base/FrontLayer/CircleMenu/CircleMenuItem.swift
@@ -31,92 +31,10 @@ class CircleMenuItem: UIButton, ShadowView, CircleView {
         self.operation = operation
         imageEdgeInsets = UIEdgeInsets(top: 6.5, left: 6.5, bottom: 6.5, right: 6.5)
 
-        switch operation {
-        case .menu:
-            setImage(image: R.image.circlemenuMenu(), color: UIColor.darkGray)
-        case .close:
-            setImage(image: R.image.circlemenuClose(), color: UIColor.darkGray)
-        case .closeAll:
-            setImage(image: R.image.circlemenuCloseAll(), color: UIColor.darkGray)
-        case .historyBack:
-            setImage(image: R.image.circlemenuHistoryback(), color: UIColor.darkGray)
-        case .copy:
-            setImage(image: R.image.circlemenuCopy(), color: UIColor.darkGray)
-        case .search:
-            setImage(image: R.image.circlemenuSearch(), color: UIColor.darkGray)
-        case .grep:
-            setImage(image: R.image.circlemenuGrep(), color: UIColor.darkGray)
-        case .add:
-            setImage(image: R.image.circlemenuAdd(), color: UIColor.darkGray)
-        case .scrollUp:
-            setImage(image: R.image.circlemenuScrollup(), color: UIColor.darkGray)
-        case .autoScroll:
-            setImage(image: R.image.circlemenuAutoscroll(), color: UIColor.darkGray)
-        case .historyForward:
-            setImage(image: R.image.circlemenuHistoryforward(), color: UIColor.darkGray)
-        case .form:
-            setImage(image: R.image.circlemenuForm(), color: UIColor.darkGray)
-        case .favorite:
-            setImage(image: R.image.circlemenuFavorite(), color: UIColor.darkGray)
-        case .urlCopy:
-            // TODO: URLコピーの画像
-            setImage(image: R.image.circlemenuFavorite(), color: UIColor.darkGray)
-        case .tabGroup:
-            // TODO: タブグループの画像
-            setImage(image: R.image.circlemenuFavorite(), color: UIColor.darkGray)
-        }
+        setImage(image: operation.image(), color: operation.imageColor())
 
         #if UT
-            switch operation {
-            case .menu:
-                accessibilityIdentifier = R.image.circlemenuMenu.name
-                log.debug("set accessibility. name: \(R.image.circlemenuMenu.name)")
-            case .close:
-                accessibilityIdentifier = R.image.circlemenuClose.name
-                log.debug("set accessibility. name: \(R.image.circlemenuClose.name)")
-            case .closeAll:
-                accessibilityIdentifier = R.image.circlemenuCloseAll.name
-                log.debug("set accessibility. name: \(R.image.circlemenuCloseAll.name)")
-            case .historyBack:
-                accessibilityIdentifier = R.image.circlemenuHistoryback.name
-                log.debug("set accessibility. name: \(R.image.circlemenuHistoryback.name)")
-            case .copy:
-                accessibilityIdentifier = R.image.circlemenuCopy.name
-                log.debug("set accessibility. name: \(R.image.circlemenuCopy.name)")
-            case .search:
-                accessibilityIdentifier = R.image.circlemenuSearch.name
-                log.debug("set accessibility. name: \(R.image.circlemenuSearch.name)")
-            case .grep:
-                accessibilityIdentifier = R.image.circlemenuGrep.name
-                log.debug("set accessibility. name: \(R.image.circlemenuGrep.name)")
-            case .add:
-                accessibilityIdentifier = R.image.circlemenuAdd.name
-                log.debug("set accessibility. name: \(R.image.circlemenuAdd.name)")
-            case .scrollUp:
-                accessibilityIdentifier = R.image.circlemenuScrollup.name
-                log.debug("set accessibility. name: \(R.image.circlemenuScrollup.name)")
-            case .autoScroll:
-                accessibilityIdentifier = R.image.circlemenuAutoscroll.name
-                log.debug("set accessibility. name: \(R.image.circlemenuAutoscroll.name)")
-            case .historyForward:
-                accessibilityIdentifier = R.image.circlemenuHistoryforward.name
-                log.debug("set accessibility. name: \(R.image.circlemenuHistoryforward.name)")
-            case .form:
-                accessibilityIdentifier = R.image.circlemenuForm.name
-                log.debug("set accessibility. name: \(R.image.circlemenuForm.name)")
-            case .favorite:
-                accessibilityIdentifier = R.image.circlemenuFavorite.name
-                log.debug("set accessibility. name: \(R.image.circlemenuFavorite.name)")
-            case .home:
-                accessibilityIdentifier = R.image.circlemenuHome.name
-                log.debug("set accessibility. name: \(R.image.circlemenuHome.name)")
-            case .urlCopy:
-                accessibilityIdentifier = R.image.circlemenuHome.name
-                log.debug("set accessibility. name: \(R.image.circlemenuHome.name)")
-            case .tabGroup:
-                accessibilityIdentifier = R.image.circlemenuHome.name
-                log.debug("set accessibility. name: \(R.image.circlemenuHome.name)")
-            }
+            accessibilityIdentifier = operation.accesibilityIdentify()
         #endif
     }
 
