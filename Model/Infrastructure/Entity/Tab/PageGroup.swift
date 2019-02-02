2173e870dc0bc2d86e371bf2910af57029484fdb
temma.io <eifan.devs@gmail.com>
2019-02-03 00:04:11 +0900

    add page group entity

---
+++ Model/Infrastructure/Entity/Tab/PageGroup.swift
@@ -0,0 +1,9 @@
+//
+//  PageGroup.swift
+//  Model
+//
+//  Created by tenma on 2019/02/02.
+//  Copyright © 2019 eifandevs. All rights reserved.
+//
+
+import Foundation
---
+++ Model/Infrastructure/Entity/Tab/PageGroupItem.swift
@@ -0,0 +1,9 @@
+//
+//  PageGroupItem.swift
+//  Model
+//
+//  Created by tenma on 2019/02/03.
+//  Copyright © 2019 eifandevs. All rights reserved.
+//
+
+import Foundation
