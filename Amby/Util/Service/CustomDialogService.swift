3b856b46c7405c76572ebd75420ce134dfb93930
temma.io <eifan.devs@gmail.com>
2019-02-25 23:38:18 +0900

    present text alert method

--- Amby/Util/Service/CustomDialogService.swift
+++ Amby/Util/Service/CustomDialogService.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+class CustomDialogService {
+    static func presentTextFieldAlert() {
+        // TODO: 処理
+        //        let alertController = UIAlertController(title: "Add New Name", message: "", preferredStyle: UIAlertControllerStyle.Alert)
+        //        alertController.addTextFieldWithConfigurationHandler { (textField : UITextField!) -> Void in
+        //            textField.placeholder = "Enter Second Name"
+        //        }
+        //        let saveAction = UIAlertAction(title: "Save", style: UIAlertActionStyle.Default, handler: { alert -> Void in
+        //            let firstTextField = alertController.textFields![0] as UITextField
+        //            let secondTextField = alertController.textFields![1] as UITextField
+        //        })
+        //        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Default, handler: {
+        //            (action : UIAlertAction!) -> Void in })
+        //        alertController.addTextFieldWithConfigurationHandler { (textField : UITextField!) -> Void in
+        //            textField.placeholder = "Enter First Name"
+        //        }
+        //
+        //        alertController.addAction(saveAction)
+        //        alertController.addAction(cancelAction)
+        //
+        //        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+}
