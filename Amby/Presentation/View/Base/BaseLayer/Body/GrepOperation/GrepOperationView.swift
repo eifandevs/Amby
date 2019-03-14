59152c870608ed0547e4740450dc29018f246b52
temma.io <eifan.devs@gmail.com>
2019-03-15 08:47:43 +0900

    add grep operation xib

---
+++ Amby/Presentation/View/Base/BaseLayer/Body/GrepOperation/GrepOperationView.swift
@@ -0,0 +1,21 @@
+//
+//  GrepOperationView.swift
+//  Amby
+//
+//  Created by tenma on 2019/03/15.
+//  Copyright © 2019 eifandevs. All rights reserved.
+//
+
+import UIKit
+
+class GrepOperationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
