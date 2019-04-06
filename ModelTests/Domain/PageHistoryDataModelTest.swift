46cbd6e1a9de15e59b44154cfea638e8b7e495a3
temma.io <eifan.devs@gmail.com>
2019-04-06 23:30:22 +0900

    add ut

--- ModelTests/Domain/PageHistoryDataModelTest.swift
+++ ModelTests/Domain/PageHistoryDataModelTest.swift
@@ -17,6 +17,11 @@ class PageHistoryDataModelTests: XCTestCase {
 
     let disposeBag = DisposeBag()
 
+    let dummyUrl = "https://abc/"
+    let dummyUrl2 = "https://def/"
+    let dummyTitle = "dummyTitle"
+    let dummyTitle2 = "dummyTitle2"
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -35,7 +40,6 @@ class PageHistoryDataModelTests: XCTestCase {
     }
 
     func testGetHistory() {
-        let dummyUrl = "https://abc/"
         PageHistoryDataModel.s.append(url: dummyUrl)
         XCTAssertTrue(PageHistoryDataModel.s.getHistory(context: PageHistoryDataModel.s.getHistory(index: 1)!.context)!.url == dummyUrl)
         XCTAssertTrue(PageHistoryDataModel.s.getHistory(index: 1)!.url == dummyUrl)
@@ -137,25 +141,18 @@ class PageHistoryDataModelTests: XCTestCase {
     }
 
     func testUpdateUrl() {
-        let dummyUrl = "https://abc/"
         PageHistoryDataModel.s.append(url: dummyUrl)
-        let dummyUrl2 = "https://def/"
         PageHistoryDataModel.s.updateUrl(context: PageHistoryDataModel.s.getHistory(index: 1)!.context, url: dummyUrl2)
         XCTAssertTrue(PageHistoryDataModel.s.getHistory(index: 1)!.url == dummyUrl2)
     }
 
     func testUpdateTitle() {
-        let dummyUrl = "https://abc/"
-        let dummyTitle = "dummy"
         PageHistoryDataModel.s.append(url: dummyUrl, title: dummyTitle)
-        let dummyTitle2 = "dummy2"
         PageHistoryDataModel.s.updateTitle(context: PageHistoryDataModel.s.getHistory(index: 1)!.context, title: dummyTitle2)
         XCTAssertTrue(PageHistoryDataModel.s.getHistory(index: 1)!.title == dummyTitle2)
     }
 
     func testInsert() {
-        let dummyUrl = "https://abc/"
-        let dummyTitle = "dummy"
         PageHistoryDataModel.s.append(url: dummyUrl, title: dummyTitle)
         PageHistoryDataModel.s.append(url: dummyUrl, title: dummyTitle)
         PageHistoryDataModel.s.insert(url: dummyUrl, title: nil)
@@ -166,12 +163,12 @@ class PageHistoryDataModelTests: XCTestCase {
             .subscribe { object in
                 if let action = object.element, case let .insert(before, after) = action {
                     if let expectation = expectation {
-                        XCTAssertTrue(before.pageHistory.url == dummyUrl)
-                        XCTAssertTrue(before.pageHistory.title == dummyTitle)
+                        XCTAssertTrue(before.pageHistory.url == self.dummyUrl)
+                        XCTAssertTrue(before.pageHistory.title == self.dummyTitle)
                         XCTAssertTrue(after.pageHistory.url == "")
-                        XCTAssertTrue(after.pageHistory.title == dummyTitle)
+                        XCTAssertTrue(after.pageHistory.title == self.dummyTitle)
                         XCTAssertTrue(PageHistoryDataModel.s.getHistory(index: 2)!.url == "")
-                        XCTAssertTrue(PageHistoryDataModel.s.getHistory(index: 2)!.title == dummyTitle)
+                        XCTAssertTrue(PageHistoryDataModel.s.getHistory(index: 2)!.title == self.dummyTitle)
                         expectation.fulfill()
                     }
                 }
@@ -191,7 +188,7 @@ class PageHistoryDataModelTests: XCTestCase {
             .subscribe { object in
                 if let action = object.element, case let .append(before, after) = action {
                     if let expectation = expectation {
-                        XCTAssertTrue(after.pageHistory.url == "https://abc/")
+                        XCTAssertTrue(after.pageHistory.url == self.dummyUrl)
                         XCTAssertTrue(before!.pageHistory.url == "")
                         expectation.fulfill()
                     }
@@ -199,7 +196,68 @@ class PageHistoryDataModelTests: XCTestCase {
             }
             .disposed(by: disposeBag)
 
-        PageHistoryDataModel.s.append(url: "https://abc/")
+        PageHistoryDataModel.s.append(url: dummyUrl)
+
+        self.waitForExpectations(timeout: 10, handler: nil)
+    }
+
+    func testAppendGroup() {
+        weak var expectation = self.expectation(description: #function)
+
+        PageHistoryDataModel.s.rx_action
+            .subscribe { object in
+                if let action = object.element, case .appendGroup = action {
+                    if let expectation = expectation {
+                        expectation.fulfill()
+                    }
+                }
+            }
+            .disposed(by: disposeBag)
+
+        PageHistoryDataModel.s.append(url: dummyUrl)
+        PageHistoryDataModel.s.appendGroup()
+
+        self.waitForExpectations(timeout: 10, handler: nil)
+    }
+
+    func testChangeGroupTitle() {
+        weak var expectation = self.expectation(description: #function)
+
+        PageHistoryDataModel.s.rx_action
+            .subscribe { object in
+                if let action = object.element, case let .changeGroupTitle(groupContext, title) = action {
+                    if let expectation = expectation {
+                        XCTAssertTrue(title == self.dummyTitle)
+                        XCTAssertTrue(groupContext == PageHistoryDataModel.s.pageGroupList.groups[1].groupContext)
+                        expectation.fulfill()
+                    }
+                }
+            }
+            .disposed(by: disposeBag)
+
+        PageHistoryDataModel.s.append(url: dummyUrl)
+        PageHistoryDataModel.s.appendGroup()
+        PageHistoryDataModel.s.changeGroupTitle(groupContext: PageHistoryDataModel.s.pageGroupList.groups[1].groupContext, title: dummyTitle)
+
+        self.waitForExpectations(timeout: 10, handler: nil)
+    }
+
+    func testRemoveGroup() {
+        weak var expectation = self.expectation(description: #function)
+
+        PageHistoryDataModel.s.rx_action
+            .subscribe { object in
+                if let action = object.element, case .deleteGroup = action {
+                    if let expectation = expectation {
+                        expectation.fulfill()
+                    }
+                }
+            }
+            .disposed(by: disposeBag)
+
+        PageHistoryDataModel.s.append(url: dummyUrl)
+        PageHistoryDataModel.s.appendGroup()
+        PageHistoryDataModel.s.removeGroup(groupContext: PageHistoryDataModel.s.pageGroupList.groups[1].groupContext)
 
         self.waitForExpectations(timeout: 10, handler: nil)
     }
@@ -211,32 +269,28 @@ class PageHistoryDataModelTests: XCTestCase {
             .subscribe { object in
                 if let action = object.element, case let .change(before, after) = action {
                     if let expectation = expectation {
-                        XCTAssertTrue(after.pageHistory.url == "https://abc/")
-                        XCTAssertTrue(before.pageHistory.url == "https://def/")
+                        XCTAssertTrue(after.pageHistory.url == self.dummyUrl)
+                        XCTAssertTrue(before.pageHistory.url == self.dummyUrl2)
                         expectation.fulfill()
                     }
                 }
             }
             .disposed(by: disposeBag)
 
-        PageHistoryDataModel.s.append(url: "https://abc/")
-        PageHistoryDataModel.s.append(url: "https://def/")
+        PageHistoryDataModel.s.append(url: dummyUrl)
+        PageHistoryDataModel.s.append(url: dummyUrl2)
         PageHistoryDataModel.s.change(context: PageHistoryDataModel.s.histories[1].context)
 
         self.waitForExpectations(timeout: 10, handler: nil)
     }
 
     func testStore() {
-        let dummyUrl = "https://abc/"
-        let dummyTitle = "dummy"
         PageHistoryDataModel.s.append(url: dummyUrl, title: dummyTitle)
         PageHistoryDataModel.s.append(url: dummyUrl, title: dummyTitle)
         PageHistoryDataModel.s.store()
     }
 
     func testDelete() {
-        let dummyUrl = "https://abc/"
-        let dummyTitle = "dummy"
         PageHistoryDataModel.s.append(url: dummyUrl, title: dummyTitle)
         PageHistoryDataModel.s.append(url: dummyUrl, title: dummyTitle)
         PageHistoryDataModel.s.store()
