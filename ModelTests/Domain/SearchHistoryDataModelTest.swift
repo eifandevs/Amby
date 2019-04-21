f743ec942ae1511bc5ccb111ff769ee129cb8939
temma.io <eifan.devs@gmail.com>
2019-04-21 14:16:48 +0900

    add ut

--- ModelTests/Domain/SearchHistoryDataModelTest.swift
+++ ModelTests/Domain/SearchHistoryDataModelTest.swift
@@ -25,13 +25,61 @@ class SearchHistoryDataModelTest: XCTestCase {
         return SearchHistoryDataModel.s
     }
 
+    var todayString: String {
+        let formatter = DateFormatter()
+        let jaLocale = Locale(identifier: "ja_JP")
+        formatter.locale = jaLocale
+        formatter.dateFormat = "yyyyMMdd"
+        return formatter.string(from: Date())
+    }
+
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         searchHistoryDataModel.delete()
+    }
+
+    func testGetList() {
+        searchHistoryDataModel.store(text: dummyText)
+        let list = searchHistoryDataModel.getList()
+        XCTAssertTrue(list.first! == todayString)
     }
 
     func testStore() {
         searchHistoryDataModel.store(text: dummyText)
     }
 
+    func testStoreWithHistories() {
+        let data = SearchHistory(title: dummyText, date: Date())
+        let data2 = SearchHistory(title: dummyText2, date: Date().yesterday)
+        searchHistoryDataModel.store(histories: [data, data2])
+        let list = searchHistoryDataModel.getList()
+        XCTAssertTrue(list.count == 2)
+    }
+
+    func testSelectWithDuplication() {
+        let data = SearchHistory(title: dummyText, date: Date())
+        let data2 = SearchHistory(title: dummyText, date: Date().yesterday)
+        searchHistoryDataModel.store(histories: [data, data2])
+        let result = searchHistoryDataModel.select(title: dummyText, readNum: 10)
+        XCTAssertTrue(result.count == 1)
+        XCTAssertTrue(result.first!.title == dummyText)
+    }
+
+    func testSelectAsc() {
+        let data = SearchHistory(title: dummyText, date: Date())
+        let data2 = SearchHistory(title: dummyText2, date: Date().yesterday)
+        searchHistoryDataModel.store(histories: [data, data2])
+        let result = searchHistoryDataModel.select(title: dummyText, readNum: 10)
+        XCTAssertTrue(result.count == 2)
+        XCTAssertTrue(result.first!.title == dummyText2)
+    }
+
+    func testSelectDesc() {
+        let data = SearchHistory(title: dummyText2, date: Date())
+        let data2 = SearchHistory(title: dummyText, date: Date().yesterday)
+        searchHistoryDataModel.store(histories: [data, data2])
+        let result = searchHistoryDataModel.select(title: dummyText, readNum: 10)
+        XCTAssertTrue(result.count == 2)
+        XCTAssertTrue(result.first!.title == dummyText)
+    }
 }
