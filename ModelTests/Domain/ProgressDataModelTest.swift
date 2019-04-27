baf4182988c5917a5b25d79d8d8a238a2fb76f18
temma.io <eifan.devs@gmail.com>
2019-04-27 23:40:26 +0900

    add ut

--- ModelTests/Domain/ProgressDataModelTest.swift
+++ ModelTests/Domain/ProgressDataModelTest.swift
@@ -7,3 +7,87 @@
 //
 
 import Foundation
+import XCTest
+import RxSwift
+import RxCocoa
+
+@testable import Model
+@testable import Entity
+
+class ProgressDataModelTest: XCTestCase {
+
+    let dummyUrl = "https://abc/"
+    let dummyUrl2 = "https://def/"
+    let dummyTitle = "dummyTitle"
+    let dummyTitle2 = "dummyTitle2"
+
+    var progressDataModel: ProgressDataModelProtocol {
+        return ProgressDataModel.s
+    }
+
+    let disposeBag = DisposeBag()
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    func testUpdateProgress() {
+        weak var expectation = self.expectation(description: #function)
+
+        progressDataModel.rx_action
+            .subscribe { object in
+                if let action = object.element, case let .updateProgress(progress) = action {
+                    if let expectation = expectation {
+                        XCTAssertTrue(progress == 0.5)
+                        expectation.fulfill()
+                    }
+                }
+            }
+            .disposed(by: disposeBag)
+
+        progressDataModel.updateProgress(progress: 0.5)
+
+        self.waitForExpectations(timeout: 10, handler: nil)
+    }
+
+    func testUpdateText() {
+        weak var expectation = self.expectation(description: #function)
+
+        progressDataModel.rx_action
+            .subscribe { object in
+                if let action = object.element, case let .updateText(text) = action {
+                    if let expectation = expectation {
+                        XCTAssertTrue(text == #function)
+                        expectation.fulfill()
+                    }
+                }
+            }
+            .disposed(by: disposeBag)
+
+        progressDataModel.updateText(text: #function)
+
+        self.waitForExpectations(timeout: 10, handler: nil)
+    }
+
+    func testReload() {
+        let tab = Tab(url: dummyUrl, title: dummyTitle)
+
+        weak var expectation = self.expectation(description: #function)
+
+        progressDataModel.rx_action
+            .subscribe { object in
+                if let action = object.element, case let .updateText(text) = action {
+                    if let expectation = expectation {
+                        XCTAssertTrue(text == self.dummyUrl)
+                        expectation.fulfill()
+                    }
+                }
+            }
+            .disposed(by: disposeBag)
+
+        progressDataModel.reload(currentTab: tab)
+
+        self.waitForExpectations(timeout: 10, handler: nil)
+    }
+}
