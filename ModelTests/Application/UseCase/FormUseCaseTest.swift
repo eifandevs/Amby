fb53233de716b911d9a3fccebd9665127c128949
temma.io <eifan.devs@gmail.com>
2019-04-30 21:51:20 +0900

    add ut

--- ModelTests/Application/UseCase/FormUseCaseTest.swift
+++ ModelTests/Application/UseCase/FormUseCaseTest.swift
@@ -1,8 +1,8 @@
 //
-//  ProgressDataModelTest.swift
+//  FormUseCaseTest.swift
 //  ModelTests
 //
-//  Created by iori tenma on 2019/04/22.
+//  Created by iori tenma on 2019/04/30.
 //  Copyright © 2019 eifandevs. All rights reserved.
 //
 
@@ -14,15 +14,14 @@ import RxCocoa
 @testable import Model
 @testable import Entity
 
-class ProgressDataModelTest: XCTestCase {
-
+class FormUseCaseTest: XCTestCase {
     let dummyUrl = "https://abc/"
     let dummyUrl2 = "https://def/"
     let dummyTitle = "dummyTitle"
     let dummyTitle2 = "dummyTitle2"
 
-    var progressDataModel: ProgressDataModelProtocol {
-        return ProgressDataModel.s
+    var formUseCase: FormUseCase {
+        return FormUseCase.s
     }
 
     let disposeBag = DisposeBag()
@@ -30,42 +29,59 @@ class ProgressDataModelTest: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        formUseCase.delete()
     }
 
-    func testUpdateProgress() {
+    func testRegisterForm() {
         weak var expectation = self.expectation(description: #function)
 
-        progressDataModel.rx_action
+        formUseCase.rx_action
             .subscribe { object in
-                if let action = object.element, case let .updateProgress(progress) = action {
+                if let action = object.element, case .register = action {
                     if let expectation = expectation {
-                        XCTAssertTrue(progress == 0.5)
                         expectation.fulfill()
                     }
                 }
             }
             .disposed(by: disposeBag)
 
-        progressDataModel.updateProgress(progress: 0.5)
+        formUseCase.registerForm()
 
         self.waitForExpectations(timeout: 10, handler: nil)
     }
 
-    func testUpdateText() {
+    func testAutofill() {
         weak var expectation = self.expectation(description: #function)
 
-        progressDataModel.rx_action
+        formUseCase.rx_action
             .subscribe { object in
-                if let action = object.element, case let .updateText(text) = action {
+                if let action = object.element, case .autoFill = action {
                     if let expectation = expectation {
-                        XCTAssertTrue(text == #function)
                         expectation.fulfill()
                     }
                 }
             }
             .disposed(by: disposeBag)
 
-        progressDataModel.updateText(text: #function)
+        formUseCase.autoFill()
+
+        self.waitForExpectations(timeout: 10, handler: nil)
+    }
+
+    func testLoad() {
+        weak var expectation = self.expectation(description: #function)
+
+        formUseCase.rx_action
+            .subscribe { object in
+                if let action = object.element, case .load = action {
+                    if let expectation = expectation {
+                        expectation.fulfill()
+                    }
+                }
+            }
+            .disposed(by: disposeBag)
+
+        formUseCase.load(url: dummyUrl)
 
         self.waitForExpectations(timeout: 10, handler: nil)
     }
