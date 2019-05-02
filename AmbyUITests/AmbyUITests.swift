af0a8332bef13d0b1de55f22e6306fb9726bf58b
temma.io <eifan.devs@gmail.com>
2019-05-02 22:48:52 +0900

    add spring board

--- AmbyUITests/AmbyUITests.swift
+++ AmbyUITests/AmbyUITests.swift
@@ -12,6 +12,7 @@ class AmbyUITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        Springboard.deleteMyApp()
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
