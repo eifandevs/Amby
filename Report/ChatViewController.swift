50484f8ff4374a5de3d9c78342fdf789f9129fe8
temma.io <eifan.devs@gmail.com>
2019-05-15 22:25:58 +0900

    add report page loading

--- Report/ChatViewController.swift
+++ Report/ChatViewController.swift
@@ -7,24 +7,19 @@
 //
 
 import UIKit
+import WebKit
 
-class ChatViewController: UIViewController {
+public class ChatViewController: UIViewController {
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let webView = WKWebView(frame: CGRect(origin: CGPoint.zero, size: view.bounds.size))
+        view.addSubview(webView)
+        let path = Bundle.main.privateFrameworksPath! + "/Report.framework/dist/index.html"
+        let url = URL(fileURLWithPath: path)
+        webView.loadFileURL(url, allowingReadAccessTo: url)
+        let request = URLRequest(url: url)
+        webView.load(request)
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
