72e2387fe8733d8c37d28e9981d241fd8a30016b
temma.io <eifan.devs@gmail.com>
2019-06-15 22:24:41 +0900

    add sync controller

---
+++ Amby/Presentation/View/Sync/SyncViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SyncViewController.swift
+//  Amby
+//
+//  Created by iori tenma on 2019/06/15.
+//  Copyright © 2019 eifandevs. All rights reserved.
+//
+
+import UIKit
+
+class SyncViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
